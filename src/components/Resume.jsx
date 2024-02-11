b4c333218850fe283ed72bf89fd0cbb10c2104da
Kwinn-Korth <kjkorth12@gmail.com>
2024-02-11 00:59:48 -0700

    added the first part of my resume stuff

---
+++ src/components/Resume.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Needs to be updated with resume information and link and possibly a photo
+const Resume = () => {
+    return (
+        <div className='resume-container'>
+            <h2>Resume</h2>
+            <p>
+                A link to my resume is coming soon!
+            </p>
+        </div>
+    );
+}
+
+export default Resume;
