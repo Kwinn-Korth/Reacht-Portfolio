d268f4607a810ba6eda6f13ae734358d60bae82e
Kwinn-Korth <kjkorth12@gmail.com>
2024-02-11 02:26:39 -0700

    added Footer stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,17 @@
 // need to figure out what to put here. Maybe just a simple footer with a link to the github repo and 
 // coming soon for Linkedin?
+import React from 'react';
+
+function Footer(){
+    return (
+        <footer>
+            <div className='footer-container'>
+                <a href='https://github.com/Kwinn-Korth' target='_blank' rel='noopener noreferrer'>
+                    Github
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
