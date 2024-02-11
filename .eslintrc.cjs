53daf4b44c71b7ef0d62440c8a97f83f25e75878
Kwinn-Korth <kjkorth12@gmail.com>
2024-02-11 01:15:38 -0700

    refracted esl, no longer throwing error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
     env: {browser: true, es2021: true},
     extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:react/jsx-runtime', //'plugin:prettier/recommended'
 ],
-    parserOptions: {ecmaFeatures: {jsx: true}, ecmaVersion: 12, sourceType: 'module'},
+    parserOptions: {ecmaFeatures: 'latest', sourceType: 'module'},
     settings: {react: {version: 'detect'}},
     plugins: ['react-refresh'],
     rules: {
